92ac8a372c4b0e31a57f9db883cde14de8a1ad90
MSagGik <smw.pantheon@bk.ru>
2024-07-07 17:08:29 +0300

    Start project

---
+++ app/src/main/java/com/msaggik/swapiapp/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.msaggik.swapiapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.msaggik.swapiapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private val navController: NavController by lazy {
+        (supportFragmentManager.findFragmentById(R.id.root_fragments) as NavHostFragment).navController
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        binding.bottomNavigationMenu.setupWithNavController(navController)
+    }
+}
